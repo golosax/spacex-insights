c059205a1ab068f78cb2de69029c5020dca5f43f
golosax <oleksii.golosun@1und1.de>
2021-04-13 23:00:49 +0200

    js tests added

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import {render} from '@testing-library/react';
+import fetch from "jest-fetch-mock"
 import App from './App';
+import RocketList from "./RocketList";
 
-test('renders learn react link', () => {
-    const {getByText} = render(<App/>);
-    const linkElement = getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+test('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+test('renders RocketList with empty rocket data', () => {
+    const component = render(<RocketList/>);
+    const customerComponent = component.getAllByText("Loading...");
+    expect(customerComponent).not.toBeNull();
 });
